4332ed73140d67f68d4a0bbb1eee9236e4abe507
Niladri <niladrisengit12@gmail.com>
2024-11-29 13:51:27 +0530

    add user modal and drawer

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,16 +1,22 @@
-import { Table } from "antd"
-import { lazy, Suspense } from "react"
-import { useAllUsersDataFetch } from "../../hooks/useAllUsersDataFetch"
-import { columns } from "./utils/Columns"
-import Fallback from "../../components/common/Fallback"
-import { useAuthStore } from "../../store/store"
+import { Button, Table } from "antd"
+import { lazy, Suspense, useState } from "react"
 import { Navigate } from "react-router-dom"
+import Fallback from "../../components/common/Fallback"
+import { useAllUsersDataFetch } from "../../hooks/useAllUsersDataFetch"
+import { useAuthStore } from "../../store/store"
+
+import { columns } from "./utils/Columns"
+/* const CreateUserDrawer = lazy(() => import("./_components/CreateUserDrawer")) */
+const CreateUserModal = lazy(() => import("./_components/CreateUserModal"))
 const UserFilter = lazy(() => import("./_components/UserFilter"))
 const BreadCrumb = lazy(() => import("./_components/BreadCrumb"))
 
 const Users = () => {
   const { data } = useAllUsersDataFetch()
   const { user } = useAuthStore()
+  const [open, setOpen] = useState(false)
+  const [loading, setLoading] = useState(false)
+
   if (user?.roles !== "ADMIN") return <Navigate to="/" replace={true} />
   return (
     <Suspense fallback={<Fallback label={"User data"} />}>
@@ -19,9 +25,22 @@ const Users = () => {
         onFilterChange={(filterName, filterValue) => {
           console.log(filterName, filterValue)
         }}
-      />
+      >
+        <Button
+          type="primary"
+          onClick={() => {
+            setLoading(true)
+            setOpen(true)
+            setTimeout(() => {
+              setLoading(false)
+            }, 2000)
+          }}
+        >
+          + Create User
+        </Button>
+      </UserFilter>
       <Table
-      rowKey={"id"}
+        rowKey={"id"}
         pagination={{
           position: ["bottomRight"],
           pageSize: 6,
@@ -29,6 +48,13 @@ const Users = () => {
         columns={columns}
         dataSource={data?.data?.users}
       />
+      {/*       <CreateUserDrawer {...{ open, setOpen }} />
+       */}{" "}
+      <CreateUserModal {...{ open, setOpen, loading }}>
+        <Button type="primary" onClick={() => setOpen(!open)}>
+          Submit
+        </Button>
+      </CreateUserModal>
     </Suspense>
   )
 }
--- src/pages/users/_components/UserFilter.tsx
+++ src/pages/users/_components/UserFilter.tsx
@@ -1,71 +1,74 @@
-import { Button, Card, Col, Input, Row, Select } from "antd"
-import { ChangeEvent, memo } from "react"
+import { Card, Col, Input, Row, Select } from "antd"
+import { ChangeEvent, memo, ReactNode } from "react"
 import { roleFilter, statusFilter } from "../utils/filterData"
 
 interface UserFilterProps {
   onFilterChange: (filterName: string, filterValue: string) => void
+  children: ReactNode
 }
 
-const UserFilter = memo(({ onFilterChange }: UserFilterProps) => {
-  return (
-    <Card
-      style={{
-        margin: "10px 0 16px 0",
-        borderRadius: "8px",
-        boxShadow: "0px 1px 1px rgba(0, 0, 0, 0.1)",
-      }}
-    >
-      <Row justify={"space-between"}>
-        <Col span={8}>
-          <Row justify={"space-between"} align={"middle"} gutter={16}>
-            <Col>
-              <Input.Search
-              allowClear
-                placeholder="Search user"
-                onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                  onFilterChange("searchByName", e.target.value)
-                }
-              />
-            </Col>
-            <Col>
-              <Select
-              allowClear
-                defaultValue="ALL"
-                style={{ width: 120 }}
-                onChange={(selectedValue: string) =>
-                  onFilterChange("roleFilter", selectedValue)
-                }
-              >
-                {roleFilter.map((role) => (
-                  <Select.Option key={role?.key} value={role?.value}>
-                    {role?.label}
-                  </Select.Option>
-                ))}
-              </Select>
-            </Col>
-            <Col>
-              <Select
-              allowClear
-                defaultValue="all"
-                style={{ width: 120 }}
-                onChange={(selectedValue: string) =>
-                  onFilterChange("statusFilter", selectedValue)
-                }
-              >
-                {statusFilter.map((status) => (
-                  <Select.Option key={status?.key} value={status?.value}>
-                    {status?.label}
-                  </Select.Option>
-                ))}
-              </Select>
-            </Col>
-          </Row>
-        </Col>
-        <Col>
-          <Button type="primary">+ Create User</Button>
-        </Col>
-      </Row>
-    </Card>
-  )
-})
+const UserFilter = memo(
+  ({ onFilterChange,children }: UserFilterProps) => {
+    return (
+      <Card
+        style={{
+          margin: "10px 0 16px 0",
+          borderRadius: "8px",
+          boxShadow: "0px 1px 1px rgba(0, 0, 0, 0.1)",
+        }}
+      >
+        <Row justify={"space-between"}>
+          <Col span={8}>
+            <Row justify={"space-between"} align={"middle"} gutter={16}>
+              <Col>
+                <Input.Search
+                  allowClear
+                  placeholder="Search user"
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    onFilterChange("searchByName", e.target.value)
+                  }
+                />
+              </Col>
+              <Col>
+                <Select
+                  allowClear
+                  defaultValue="ALL"
+                  style={{ width: 120 }}
+                  onChange={(selectedValue: string) =>
+                    onFilterChange("roleFilter", selectedValue)
+                  }
+                >
+                  {roleFilter.map((role) => (
+                    <Select.Option key={role?.key} value={role?.value}>
+                      {role?.label}
+                    </Select.Option>
+                  ))}
+                </Select>
+              </Col>
+              <Col>
+                <Select
+                  allowClear
+                  defaultValue="all"
+                  style={{ width: 120 }}
+                  onChange={(selectedValue: string) =>
+                    onFilterChange("statusFilter", selectedValue)
+                  }
+                >
+                  {statusFilter.map((status) => (
+                    <Select.Option key={status?.key} value={status?.value}>
+                      {status?.label}
+                    </Select.Option>
+                  ))}
+                </Select>
+              </Col>
+            </Row>
+          </Col>
+          <Col>
+           {children}
+          </Col>
+        </Row>
+      </Card>
+    )
+  }
+)
 export default UserFilter
