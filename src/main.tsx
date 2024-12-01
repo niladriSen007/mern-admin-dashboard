257e8f6e2bc72a6ee5d7e7ba95a9a964b483ec3c
Niladri <niladrisengit12@gmail.com>
2024-12-01 16:24:42 +0530

    upgraded to react router v7

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ConfigProvider } from "antd"
 import "antd/dist/reset.css"
 import { createRoot } from "react-dom/client"
-import { RouterProvider } from "react-router-dom"
+import ErrorBoundary from "./components/errorBoundary/ErrorBoundary.tsx"
 import "./index.css"
 import { router } from "./router/router.tsx"
-import ErrorBoundary from "./components/errorBoundary/ErrorBoundary.tsx"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 const queryClient = new QueryClient()
 
@@ -20,7 +19,7 @@ createRoot(document.getElementById("root")!).render(
           },
         }}
       >
-        <RouterProvider router={router} />
+        {router}
       </ConfigProvider>
     </QueryClientProvider>
   </ErrorBoundary>
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,48 +1,31 @@
-import { createBrowserRouter } from "react-router-dom"
 
-import { LoginPage, AuthLayout, Dashboard, UnauthLayout,RootLayout,Users } from "./index"
 import Restaurants from "../pages/restaurants/Restaurants"
 import SingleRestaurant from "../pages/restaurants/id/SingleRestaurant"
+import {
+  AuthLayout,
+  Dashboard,
+  LoginPage,
+  RootLayout,
+  UnauthLayout,
+  Users,
+} from "./index"
 
+import { BrowserRouter, Route, Routes } from "react-router"
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-    children: [
-      {
-        path: "/",
-        element: <AuthLayout />,
-        children: [
-          {
-            path: "/",
-            element: <Dashboard />,
-          },
-          {
-            path: "/users",
-            element: <Users />,
-          },
-          {
-            path: "/restaurants/:id",
-            element: <SingleRestaurant />
-          },
-          {
-            path: "/restaurants",
-            element: <Restaurants />,
-          },
-         
-        ],
-      },
-      {
-        path: "/unauth",
-        element: <UnauthLayout />,
-        children: [
-          {
-            path: "/unauth/login",
-            element: <LoginPage />,
-          },
-        ],
-      },
-    ],
-  },
-])
+export const router = (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<RootLayout />}>
+        <Route path="/" element={<AuthLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="users" element={<Users />} />
+          <Route path="restaurants/:id" element={<SingleRestaurant />} />
+          <Route path="restaurants" element={<Restaurants />} />
+        </Route>
+        <Route path="unauth" element={<UnauthLayout />}>
+          <Route path="login" element={<LoginPage />} />
+        </Route>
+      </Route>
+    </Routes>
+  </BrowserRouter>
+)
